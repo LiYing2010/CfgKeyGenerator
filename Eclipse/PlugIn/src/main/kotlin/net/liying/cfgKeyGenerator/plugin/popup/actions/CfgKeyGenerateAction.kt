10544cf85a9d00d897460625bf1565fcbf7e0f68
LiYing <liying.cn.2010@gmail.com>
2016-07-27 10:35:31 +0900

    show runtime error

--- Eclipse/PlugIn/src/main/kotlin/net/liying/cfgKeyGenerator/plugin/popup/actions/CfgKeyGenerateAction.kt
+++ Eclipse/PlugIn/src/main/kotlin/net/liying/cfgKeyGenerator/plugin/popup/actions/CfgKeyGenerateAction.kt
@@ -1,19 +1,23 @@
 package net.liying.cfgKeyGenerator.plugin.popup.actions
 
 import net.liying.cfgKeyGenerator.generator.CfgKeyGenerator
+import net.liying.cfgKeyGenerator.plugin.Activator
 import net.liying.cfgKeyGenerator.plugin.ui.dialog.GenerateInfoDialog
-
 import org.eclipse.core.resources.IFile
-
+import org.eclipse.core.runtime.IStatus
+import org.eclipse.core.runtime.MultiStatus
+import org.eclipse.core.runtime.Status
 import org.eclipse.jface.action.IAction
+import org.eclipse.jface.dialogs.ErrorDialog
 import org.eclipse.jface.viewers.ISelection
 import org.eclipse.jface.viewers.IStructuredSelection
-
 import org.eclipse.jface.window.Window
-
+import org.eclipse.swt.widgets.Shell
 import org.eclipse.ui.IObjectActionDelegate
 import org.eclipse.ui.IWorkbenchPart
 import org.eclipse.ui.IWorkbenchPartSite
+import java.io.PrintWriter
+import java.io.StringWriter
 
 class CfgKeyGenerateAction : IObjectActionDelegate {
 	private var site: IWorkbenchPartSite? = null
@@ -47,15 +51,37 @@ class CfgKeyGenerateAction : IObjectActionDelegate {
 			return
 		}
 
-		var shell = this.site?.workbenchWindow?.shell
-		val dialog = GenerateInfoDialog(shell)
 
-		dialog.create()
-		dialog.cfgFile = selectedFile
-		dialog.loadInitData()
+		try {
+			val dialog = GenerateInfoDialog(this.getShell())
 
-		if (dialog.open() == Window.OK) {
-			CfgKeyGenerator.generate(dialog.resultParams)
+			dialog.create()
+			dialog.cfgFile = selectedFile
+			dialog.loadInitData()
+
+			if (dialog.open() == Window.OK) {
+				CfgKeyGenerator.generate(dialog.resultParams)
+			}
+		} catch(e: Throwable) {
+			this.showError(e)
 		}
+	}
+
+	private fun getShell(): Shell? = this.site?.workbenchWindow?.shell
+
+	private fun showError(error: Throwable) {
+		val stringWriter = StringWriter()
+		val printWriter = PrintWriter(stringWriter)
+		error.printStackTrace(printWriter)
+		printWriter.flush()
+
+		val childStatusList = mutableListOf<Status>()
+		stringWriter.toString().lineSequence().forEach { errorLine ->
+			childStatusList.add(Status(IStatus.ERROR, Activator.PLUGIN_ID, errorLine))
+		}
+
+		val status = MultiStatus(Activator.PLUGIN_ID, IStatus.ERROR,
+				childStatusList.toTypedArray(), error.message, error)
+		ErrorDialog.openError(this.getShell(), "Error", error.message, status);
 	}
 }
